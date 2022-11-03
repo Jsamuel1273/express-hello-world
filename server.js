a5583dc3d3a1f5b94a99570678cf1f32fea39dfe
Jsamuel1273 <109645212+Jsamuel1273@users.noreply.github.com>
2022-11-03 16:19:20 +1100

    Update server.js
    dklkldk

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./app')
 
 const port = process.env.PORT || 3000
+app.get("/", (req, res)=>{
+res.sedn("<h1>Hello world</h1>")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
